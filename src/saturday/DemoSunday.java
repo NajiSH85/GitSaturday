7c8cb3d56eecccb23065decbfabbe0517315c539
shams <shams@10.0.0.5>
2021-10-02 08:09:13 -0700

    hi

--- src/saturday/DemoSunday.java
+++ src/saturday/DemoSunday.java
@@ -6,8 +6,7 @@ public class DemoSunday {
 		System.out.println("Sunday Demooo ...");
 		System.out.println(" XYZ ");
 		
-	}
-	//By Shams
-	public static void addNum(int a, int b){
 		
+		//By EClips
+	}
 }
