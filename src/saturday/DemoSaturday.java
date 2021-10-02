bef1141813bf0cf9616a39456c3c2807f9e4e274
NajiSH85 <91561807+NajiSH85@users.noreply.github.com>
2021-10-02 10:00:14 -0700

    Update DemoSaturday.java

--- src/saturday/DemoSaturday.java
+++ src/saturday/DemoSaturday.java
@@ -3,5 +3,8 @@ package saturday;
 public class DemoSaturday {
 	public static void main(String[]args) {
 		System.out.println("Saturday Demooo ...");
+		
+		//By Github
+		System.out.println("Hello this is shams...");
 	}
 }
