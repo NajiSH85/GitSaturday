becc57b800c653da55b505a5617ef7503584cff2
NajiSH85 <91561807+NajiSH85@users.noreply.github.com>
2021-10-02 08:06:34 -0700

    Update DemoSunday.java
    By Shams

--- src/saturday/DemoSunday.java
+++ src/saturday/DemoSunday.java
@@ -5,5 +5,9 @@ public class DemoSunday {
 	public static void main(String[]args) {
 		System.out.println("Sunday Demooo ...");
 		System.out.println(" XYZ ");
+		
 	}
+	//By Shams
+	public static void addNum(int a, int b){
+		
 }
